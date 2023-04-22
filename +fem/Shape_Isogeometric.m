ee46c6d2595d69fdcbb74c28ed46e68e2d6b72b7
João Carlos Peixoto <joaoclpeixoto@gmail.com>
2023-04-22 17:29:52 +0000

    Replace Shape_Isogeometric.m

--- +fem/Shape_Isogeometric.m
+++ +fem/Shape_Isogeometric.m
@@ -25,56 +25,29 @@ classdef Shape_Isogeometric < fem.Shape
     methods
         %------------------------------------------------------------------
         function this = Shape_Isogeometric(nodes)
-            nno = length(nodes);
-            this = this@fem.Shape(fem.Shape.ISOGe,2,nno);
+            this = this@fem.Shape(fem.Shape.ISOGe,2,length(nodes),8);
             
             if (nargin > 0)
                 this.nodes = nodes;
-                this.nep = 8;
                 
-                % Cartesian control points coordiantes matrix [X Y] and
-                % weights matrix
-                X = zeros(nno,1);
-                Y = zeros(nno,1);
-                weights = zeros(nno,1);
-                for i = 1:nno
-                    X(i) = nodes(i).coord(1);
-                    Y(i) = nodes(i).coord(2);
-                    weights(i) = nodes(i).weight;
+                % Cartesian control points coordiantes matrix [X Y]
+                for i = 1:this.nen
+                    this.carCoord(i,:) = [nodes(i).coord(1) nodes(i).coord(2)];
                 end
-                carCoord = [X, Y];
-                this.carCoord = carCoord;
-                this.weights = weights;
                 
                 % Parametric nodal coordinates matrix [r s]
-                tol = 1e-10;
-                this.parCoord = [ -1+tol -1+tol;
-                                   1-tol -1+tol;
-                                   1-tol  1-tol;
-                                  -1+tol  1-tol;
-                                   0     -1+tol;
-                                   1-tol  0;
-                                   0      1-tol;
-                                  -1+tol  0 ];
-                              
-                % Set zeros at extrapolation points cartesian coordinates
-                this.epCarCoord = zeros(this.nep,2);
+                tol = 1e-8;
+                this.parCoord = [-1+tol -1+tol;
+                                  1-tol -1+tol;
+                                  1-tol  1-tol;
+                                 -1+tol  1-tol;
+                                  0     -1+tol;
+                                  1-tol  0;
+                                  0      1-tol;
+                                 -1+tol  0];
 
-                % Vector of local node ids in ccw order
-                this.ccwLocalNodeIds = [ 1  5  2  6  3  7  4  8 ];
-                
-                % Vector of global node ids in ccw order
-                this.ccwNodeIds = ...
-                [ nodes(1).id  nodes(5).id  nodes(2).id  nodes(6).id ...
-                  nodes(3).id  nodes(7).id  nodes(4).id  nodes(8).id];
-                
-                % Area
-                x = this.carCoord(1:4,1);
-                y = this.carCoord(1:4,2);
-                this.size = polyarea(x,y);
-                
-                % Characteristic length
-                this.Lchr = this.size^(1/2);
+                % Vector of local extrapolation nodes ids in ccw order
+                this.ccwLocalExtNodeIds = [1  5  2  6  3  7  4  8];
             end
         end
     end
@@ -82,7 +55,7 @@ classdef Shape_Isogeometric < fem.Shape
     %% Public methods
     % Implementation of the abstract methods declared in super-class Shape
     methods
-        function setEPoints(this,xiSpan,etaSpan,surface)
+        function setExtNodesCoord(this,xiSpan,etaSpan,surface)
             for i = 1:this.nep
                 % Extrapolation points parent coordinates
                 r = this.parCoord(i,1);
@@ -93,45 +66,54 @@ classdef Shape_Isogeometric < fem.Shape
                 eta = this.parent2ParametricSpace(etaSpan,s);
                 
                 % Surface properties
-                p = surface.degreeU;
-                q = surface.degreeV;
-                knotVectorU = surface.knotVectorU;
-                knotVectorV = surface.knotVectorV;
+                p = surface.degreeXi;
+                q = surface.degreeEta;
+                knotVectorXi = surface.knotVectorXi;
+                knotVectorEta = surface.knotVectorEta;
                 weights = surface.weights;
                 
                 % Basis functions
-                N = this.Nmtx(xi,eta,p,q,knotVectorU,knotVectorV,weights);
+                N = this.Nmtx(xi,eta,p,q,knotVectorXi,knotVectorEta,weights);
                 
                 % Extrapolation points cartesian coordinates
-                this.epCarCoord(i,:) = N * this.carCoord;
+                this.extCarCoord(i,:) = N * this.carCoord;
             end
         end
+        
+        %------------------------------------------------------------------
+        function setccwExtNodeIds(this,extNodeIds)
+            this.ccwExtNodeIds = ...
+            [extNodeIds(1)  extNodeIds(5)  extNodeIds(2)  extNodeIds(6)...
+             extNodeIds(3)  extNodeIds(7)  extNodeIds(4)  extNodeIds(8)];
+        end
+        
         %------------------------------------------------------------------
         % Evaluate matrix of geometry shape functions at a given position in
         % parametric coordinates.
         % Since this is an isoparametric element shape it returns the
         % evaluation of d.o.f. shape functions. 
-        function M = Mmtx(this,xi,eta,p,q,knotVectorU,knotVectorV,weights)
-            M = this.Nmtx(xi,eta,p,q,knotVectorU,knotVectorV,weights);
+        function M = Mmtx(this,xi,eta,p,q,knotVectorXi,knotVectorEta,weights)
+            M = this.Nmtx(xi,eta,p,q,knotVectorXi,knotVectorEta,weights);
         end
         
         %------------------------------------------------------------------
         % Evaluate matrix of d.o.f. shape functions at a given position in
         % parametric coordinates.
-        function N = Nmtx(~,xi,eta,p,q,knotVectorU,knotVectorV,weights)
-            addpath ders/
-            [N, ~, ~] = NURBS2DBasisDers([xi;eta],p,q,knotVectorU,knotVectorV,weights');
+        function N = Nmtx(~,xi,eta,p,q,knotVectorXi,knotVectorEta,weights)
+            addpath ders/ders2d
+            [N, ~, ~] = NURBS2DBasisDers([xi;eta],p,q,knotVectorXi,knotVectorEta,weights');
         end
         
         %------------------------------------------------------------------
         % Evaluate matrix of edge d.o.f. shape functions at a given
         % position in parametric coordinates.
-        function N = NmtxEdge(~,r)
-            N = zeros(1,3);
-            
-            N(3) = 1-r*r;
-            N(1) = 0.5*(1-r) - 0.5*N(3);
-            N(2) = 0.5*(1+r) - 0.5*N(3);
+        function N = NmtxEdge(this,xi,p,knotVector)
+            weights = zeros(this.nen,1);
+            for i = 1:this.nen
+                weights(i) = this.nodes(i).weight;
+            end
+            addpath ders/ders1d
+            [N, ~] = NURBS1DBasisDers(xi,p,knotVector,weights);
         end
         
         %------------------------------------------------------------------
@@ -139,28 +121,30 @@ classdef Shape_Isogeometric < fem.Shape
         % w.r.t. parametric coordinates at a given position.
         % Since this is an isoparametric element shape it returns the
         % evaluation of displacement shape functions derivatives. 
-        function GradMpar = gradMmtx(this,xi,eta,p,q,knotVectorU,knotVectorV,weights)
-            GradMpar = this.gradNmtx(xi,eta,p,q,knotVectorU,knotVectorV,weights);
+        function GradMpar = gradMmtx(this,xi,eta,p,q,knotVectorXi,knotVectorEta,weights)
+            GradMpar = this.gradNmtx(xi,eta,p,q,knotVectorXi,knotVectorEta,weights);
         end
         
         %------------------------------------------------------------------
         % Evaluate matrix of d.o.f. shape functions derivatives
         % w.r.t. parametric coordinates at a given position.
-        function GradNpar = gradNmtx(~,xi,eta,p,q,knotVectorU,knotVectorV,weights)
-            addpath ders/
-            [~, dNdxi, dNdeta] = NURBS2DBasisDers([xi;eta],p,q,knotVectorU,knotVectorV,weights');
+        function GradNpar = gradNmtx(~,xi,eta,p,q,knotVectorXi,knotVectorEta,weights)
+            addpath ders/ders2d
+            [~, dNdxi, dNdeta] = NURBS2DBasisDers([xi;eta],p,q,knotVectorXi,knotVectorEta,weights');
             GradNpar = [dNdxi; dNdeta];
         end
         
         %------------------------------------------------------------------
         % Evaluate matrix of edge geometry shape functions derivatives
         % w.r.t. parametric coordinates at a given position.
-        function GradMpar = gradMmtxEdge(~,r)
-            GradMpar = zeros(1,3);
-            
-            GradMpar(1,1) = -0.5 + r;
-            GradMpar(1,2) =  0.5 + r;
-            GradMpar(1,3) = -2*r;
+        function GradMpar = gradMmtxEdge(this,xi,p,knotVector)
+            weights = zeros(this.nen,1);
+            for i = 1:this.nen
+                weights(i) = this.nodes(i).weight;
+            end
+            addpath ders/ders1d
+            [~, dNdxi] = NURBS1DBasisDers(xi,p,knotVector,weights);
+            GradMpar = dNdxi;
         end
         
         %------------------------------------------------------------------
@@ -172,14 +156,15 @@ classdef Shape_Isogeometric < fem.Shape
         % The two corner nodes local ids are returned in parameters n1 and
         % n2. The local id of the mid node at the target edge, is returned
         % in the parameter mid.
-        function [valid,n1,n2,mid] = edgeLocalIds(this,corner1,corner2)
+        function [valid,edgLocIds,direc] = edgeLocalIds(this,corner1,corner2,degreeXi,degreeEta)
             valid = false;
             n1    = 0;
             n2    = 0;
-            mid   = 0;
+            edgLocIds   = [];
+            direc = "";
 
             % Get ids of corner nodes and check for consistency
-            for i = 1:4
+            for i = 1:this.nen
                 node = this.nodes(i).id;
                 if node == corner1
                     n1 = i;
@@ -191,7 +176,7 @@ classdef Shape_Isogeometric < fem.Shape
                 return;
             end
             
-            for i = 1:4
+            for i = 1:this.nen
                 node = this.nodes(i).id;
                 if node == corner2
                     n2 = i;
@@ -200,53 +185,33 @@ classdef Shape_Isogeometric < fem.Shape
             end
             
             if n2 == 0
-                n1 = 0;
                 return;
             end
            
             % Check for corner node consistency and get mid point id
-            if n1 == 1
-                if n2 == 2
-                    mid = 5;
-                elseif n2 == 4
-                    mid = 8;
-                else
-                    n1 = 0;
-                    n2 = 0;
-                    return; 
-                end
-            elseif n1 == 2
-                if n2 == 1
-                    mid = 5;
-                elseif n2 == 3
-                    mid = 6;
-                else
-                    n1 = 0;
-                    n2 = 0;
-                    return; 
-                end
-            elseif n1 == 3
-                if n2 == 2
-                    mid = 6;
-                elseif n2 == 4
-                    mid = 7;
-                else
-                    n1 = 0;
-                    n2 = 0;
-                    return; 
-                end
-            else % if n1 == 4
-                if n2 == 1
-                    mid = 8;
-                elseif n2 == 3
-                    mid = 7;
-                else
-                    n1 = 0;
-                    n2 = 0;
-                    return; 
-                end
-            end
+            %nodesIds_Vector = 1:(degreeXi+1)*(degreeEta+1);
+            %nodesIds_Matrix = reshape(nodesIds_Vector,degreeEta+1,degreeXi+1);
+            %nodesIds_Matrix = flip(nodesIds_Matrix');
             
+            nodesIds_Vector = 1:(degreeXi+1)*(degreeEta+1);
+            nodesIds_Matrix = reshape(nodesIds_Vector,degreeXi+1,degreeEta+1)';
+            nodesIds_Matrix = flipud(nodesIds_Matrix);
+            
+            [row1, column1] = find(nodesIds_Matrix==n1);
+            [row2, column2] = find(nodesIds_Matrix==n2);
+            
+            if row1 == row2
+                edgLocIds = nodesIds_Matrix(row1,:);
+                direc = "xi";
+            elseif column1 == column2 
+                edgLocIds = nodesIds_Matrix(:,column1)';
+                edgLocIds = flip(edgLocIds);
+                direc = "eta";
+            else
+                edgLocIds = [];
+                direc = "";
+                return;
+            end
             valid = true;
         end
     
