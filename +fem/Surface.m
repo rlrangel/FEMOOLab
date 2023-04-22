7f86765ac68a26d950b363d5cc2c04af6249c07b
João Carlos Peixoto <joaoclpeixoto@gmail.com>
2023-04-22 17:31:34 +0000

    Replace Surface.m

--- +fem/Surface.m
+++ +fem/Surface.m
@@ -9,14 +9,14 @@ classdef Surface < handle
     %% Public properties
     properties (SetAccess = public, GetAccess = public)
         % General properties
-        id          int32  = int32.empty;  % surface identification number
-        ctrlNet     double = double.empty; % vector of id's of control points in surface
-        elems       double = double.empty; % vector of id's of elements in surface
-        knotVectorU double = double.empty; % vector of knotvector in u direction
-        knotVectorV double = double.empty; % vector of knotvector in v direction
-        degreeU     double = double.empty; % degree in u direction
-        degreeV     double = double.empty; % degree in v direction
-        weights     double = double.empty; % weights of points in control net
+        id            int32  = int32.empty;  % surface identification number
+        ctrlNet       double = double.empty; % vector of id's of control points in surface
+        elems         double = double.empty; % vector of id's of elements in surface
+        knotVectorXi  double = double.empty; % knotvector in xi direction
+        knotVectorEta double = double.empty; % knotvector in eta direction
+        degreeXi      double = double.empty; % degree in xi direction
+        degreeEta     double = double.empty; % degree in eta direction
+        weights       double = double.empty; % weights of points in control net
     end
     
     %% Constructor method
